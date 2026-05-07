11210b8ecd6b3090c8f0122beeb42f629aad3989
Joaquin Bizcocho Cano <jbizcocho@ayesa.com>
2026-05-07 11:05:49 +0200

    Cambiamos securityConfig para poder usar la app desplegada

--- Backend/collectorhub-backend/src/main/java/com/collectorhub/backend/security/SecurityConfig.java
+++ Backend/collectorhub-backend/src/main/java/com/collectorhub/backend/security/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -27,17 +32,36 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configure(http))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        // BLOQUEO REAL: Solo los administradores pueden entrar aquí
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        // El resto de rutas requieren que estés logueado con un token válido
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 1. ¿Quién puede entrar? (Tu Vercel y tu Localhost para cuando programes)
+        configuration.setAllowedOrigins(Arrays.asList(
+                "https://collector-hub-frontend.vercel.app",
+                "http://localhost:5173", // Por si usas Vite en local
+                "http://localhost:3000"  // Por si usas React/Next en local
+        ));
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); 
+        return source;
     }
 }
